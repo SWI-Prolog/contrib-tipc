f0f57c8c2bc6509234d53decc66f44efe77d5dbe
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2026-01-23 15:31:52 +0100

    Update tipc.pl

--- tipc.pl
+++ tipc.pl
@@ -280,7 +280,7 @@ details.
 %
 %    $ conn_timeout(+Seconds) :
 %    Specifies the time interval that tipc_connect/2 will use before
-%    abandoning a connection attempt. Default: 8.000 sec, Minimum 1,
+%    abandoning a connection attempt. Default: 8.000 sec, Minimum 0,
 %    Maximum 300 sec.
 
 %!   tipc_receive(+Socket, -Data, -From, +OptionList) is det.
